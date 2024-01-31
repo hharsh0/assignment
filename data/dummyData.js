6736d0535add4cbf810ab1bb48a0e222835762fa
hharsh0 <harsh>
2024-02-01 00:09:19 +0530

    ready

---
+++ data/dummyData.js
@@ -0,0 +1,5 @@
+export const houses = [
+  { id: 1, latitude: 1.2858968208215247, longitude: 103.84778839607469 },
+  { id: 2, latitude: 1.290265382952728, longitude: 103.84522880185656 },
+  { id: 3, latitude: 1.2872030853992213, longitude: 103.85134183524086 },
+];
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,19 +1,47 @@
 import React, { useState, useContext } from "react";
 import MapView, { Circle } from "react-native-maps";
-import { StyleSheet, View, SafeAreaView,Image, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, SafeAreaView,Image, Text, TouchableOpacity, Modal, Button } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import { AuthContext } from "../store/auth-context";
-
+import { houses } from "../data/dummyData";
 
 const MainScreen = () => {
   const [circlePosition, setCirclePosition] = useState(null);
   const authCtx = useContext(AuthContext);
+  const [showModal, setShowModal] = useState(false);
+  const [houseId, setHouseId] = useState();
+
+  
 
   const handleMapPress = (e) => {
-    // Update the position whenever the user clicks on the map
     setCirclePosition(e.nativeEvent.coordinate);
+
+    houses.forEach((house) => {
+      const distance = calculateDistance(e.nativeEvent.coordinate, house);
+      if (distance < 0.002) {
+        console.log(distance)
+        sendNotificationToHouse(house.id);
+        setHouseId(house.id)
+        setShowModal(true);
+      }
+    });
   };
+
+  const calculateDistance = (coordinate1, coordinate2) => {
+    const latDiff = coordinate1.latitude - coordinate2.latitude;
+    const lonDiff = coordinate1.longitude - coordinate2.longitude;
+    return Math.sqrt(latDiff * latDiff + lonDiff * lonDiff);
+  };
+
+  const sendNotificationToHouse = (houseId) => {
+    console.log(`Sending notification to house with ID: ${houseId}`);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,7 +49,10 @@ const MainScreen = () => {
         <TouchableOpacity style={styles.icon}>
           <FontAwesome name="user-circle" size={24} color="#fff" />
         </TouchableOpacity>
-        <TouchableOpacity onClick={()=>authCtx.logout()} style={styles.profile}>
+        <TouchableOpacity
+          onClick={() => authCtx.logout()}
+          style={styles.profile}
+        >
           <Image
             style={styles.image}
             source={{
@@ -52,6 +83,19 @@ const MainScreen = () => {
       <View style={styles.bottomSheet}>
         <View style={styles.line} />
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Notification sent to House with ID: {houseId}</Text>
+            <Button style={styles.button} title="Close Modal" onPress={closeModal} />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -76,7 +120,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     zIndex: 1,
     top: 0,
-    marginTop: 30,
+    marginTop: 60,
   },
   icon: {
     backgroundColor: "#26292d",
@@ -105,17 +149,32 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     width: "100%",
-    height: 50,
+    height: 80,
     backgroundColor: "#26292d",
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
   },
   line: {
-      width: 50,
-      height: 5,
-      backgroundColor: "gray",
-      borderRadius: 10,
-      margin: 10,
-      alignSelf: "center",
+    width: 50,
+    height: 5,
+    backgroundColor: "gray",
+    borderRadius: 10,
+    margin: 10,
+    alignSelf: "center",
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  button:{
+    marginTop: 15
+  }
 });
